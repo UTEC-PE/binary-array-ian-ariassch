db1b80f3a59318bb0e404d815e09e28183ec4ec2
ian-ariassch <38139729+ian-ariassch@users.noreply.github.com>
2018-10-30 22:10:09 -0500

    biton y bitoff

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include "binary.cpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << "===========================================================" << endl;
-    cout << "\Binary Array Practice" << endl;
+    cout << "Binary Array Practice" << endl;
     cout << "===========================================================" << endl << endl;
 
-    system("read");
+
+    boolean xd(16);
+    xd.bitOff(7);
+    xd.bitOn(10);
+    xd.bitOn(11);
+    xd.print();
     return EXIT_SUCCESS;
 }
 
